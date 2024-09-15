cf72a7169b3311300b7cedcfacef30c10ea7ec2a
AbdifataH-hersi <mohamedabdifatah107@gmail.com>
2024-09-15 09:47:35 +0900

    tip calcular exercise

--- challenge.js
+++ challenge.js
@@ -1,2 +1,19 @@
 // Good Luck! You got this 💪🏾
 // Write your code here.
+
+
+let bill = 275; 
+let tip;
+
+
+if (bill >= 50 && bill <= 300) {
+    tip = bill * 0.15;
+} else {
+    tip = bill * 0.20;
+}
+
+
+let total = bill + tip;
+
+
+console.log(`The bill was ${bill}, the tip was ${tip.toFixed(2)}, and the total value ${total.toFixed(2)}`);
